574bba490b56e60fbbe2be43d92561a0b4a9976d
Pomidorka1234 <Jansafronov@gmail.com>
2021-05-21 03:55:55 +0300

    Commit Pomodoro pen

---
+++ Pens/Pomodoro Clock/pomodoro-clock.markdown
@@ -0,0 +1,7 @@
+Pomodoro Clock
+--------------
+
+
+A [Pen](https://codepen.io/Triugolnik/pen/WNpRzZO) by [Jan](https://codepen.io/Triugolnik) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/Triugolnik/pen/WNpRzZO/license).
---
+++ Pens/Pomodoro Clock/scripts.js
@@ -0,0 +1,4 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/16.13.1/umd/react.production.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.13.1/umd/react-dom.production.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xregexp/3.2.0/xregexp-all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
